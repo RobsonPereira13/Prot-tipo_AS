2bc855ba99137ccbed5b2cf3563cbd103f58350b
Rodrigo Rodrigues <rodrigo7.carvalho.rodrigues@gmail.com>
2018-08-20 00:52:27 -0300

    Telas de Login e Cadastro Alteradas, e adicionados novos ícones.

--- app/src/main/java/com/alerta/robso/acompanhamentos/Activity/Home.java
+++ app/src/main/java/com/alerta/robso/acompanhamentos/Activity/Home.java
@@ -4,19 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.alerta.robso.acompanhamentos.R;
 
 public class Home extends AppCompatActivity {
-    private ImageButton botaoCadastrese;
+    private Button botaoCadastrese;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        botaoCadastrese = (ImageButton)findViewById(R.id.botaocadastreseId);
+        botaoCadastrese = (Button)findViewById(R.id.botaocadastreseId);
 
         botaoCadastrese.setOnClickListener(new View.OnClickListener() {
             @Override
